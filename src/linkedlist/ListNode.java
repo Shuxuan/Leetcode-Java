f1b4d8eb798b6911734c6f58c4c61b576ad6ed88
Shuxuan Nie <shuxuan.nie@gmail.com>
2016-07-05 15:49:09 -0700

    ReverseKLinkedList

---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,12 @@
+package linkedlist;
+
+/**
+ * Created by shuxuannie on 7/5/16.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+}
